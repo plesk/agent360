c0a037018941d02cc13faa3a37f64e2d6827cf70
Stanislav Smirnov <ssmirnov@plesk.com>
2023-05-12 00:16:11 +0300

    TECH Bump version up to 1.2.43

--- agent360/agent360.py
+++ agent360/agent360.py
@@ -45,7 +45,7 @@ except ImportError:
     from urllib import urlencode
     from urllib2 import urlopen, Request, HTTPError
 
-__version__ = '1.2.42'
+__version__ = '1.2.43'
 __FILEABSDIRNAME__ = os.path.dirname(os.path.abspath(__file__))
 
 ini_files = (
--- windows/setup.iss
+++ windows/setup.iss
@@ -1,5 +1,5 @@
 #define MyAppName "Agent360"
-#define MyAppVersion "1.2.42"
+#define MyAppVersion "1.2.43"
 #define MyAppPublisher "360 Monitoring"
 #define MyAppURL "https://360monitoring.io/"
 
