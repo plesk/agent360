de513455a3c6868dd54e16d5c4e974b3a1e8ea91
Stanislav Smirnov <stanislav.smirnov@webpros.com>
2024-04-05 18:37:41 +0300

    TECH Bump version up to 1.2.48

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ else:
 
 setuptools.setup(
     name='agent360',
-    version='1.2.47',
+    version='1.2.48',
     description='360 agent',
     long_description_content_type='text/markdown',
     long_description=readme,
--- windows/setup.iss
+++ windows/setup.iss
@@ -1,5 +1,5 @@
 #define MyAppName "Agent360"
-#define MyAppVersion "1.2.47"
+#define MyAppVersion "1.2.48"
 #define MyAppPublisher "360 Monitoring"
 #define MyAppURL "https://360monitoring.io/"
 
